2070550ec66357b5ea729a586328e7ce6fd91082
aditya <aditya@anwisys.com>
2015-04-19 00:55:11 -0400

    Added forecast.io on home page

--- www/js/app.js
+++ www/js/app.js
@@ -91,5 +91,5 @@ angular.module('starter', ['ionic', 'starter.controllers','starter.services'])
           }
         });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/events');
+  $urlRouterProvider.otherwise('/app/info');
 });
